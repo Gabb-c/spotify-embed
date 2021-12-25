4ad77b0f9e6bdbdc356e93e4d43935c46f1ef69f
Gabb-c <gdgabrieldacunha71@gmail.com>
2021-12-25 18:31:51 -0300

    feat(build): set up rollup build

---
+++ banner.js
@@ -0,0 +1,10 @@
+export const myBanner = `
+/**
+ * - - - - - - - -
+ * ${packageJson.name}
+ * Author: ${packageJson.author.name}
+ * License: ${packageJson.license}
+ * Version: ${packageJson.version}
+ * - - - - - - - -
+ */
+`;
---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import del from 'rollup-plugin-delete';
+import banner2 from 'rollup-plugin-banner2';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+
+import * as packageJson from './package.json';
+import { myBanner } from './banner';
+
+/** @type {import('rollup').RollupOptions} */
+const config = [
+  {
+    input: 'src/index.ts',
+    output: [
+      // commonjs
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      // esmodule
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(), // automatically externalize peerDependencies in a rollup bundle
+      resolve(), // locates modules using the Node resolution algorithm
+      commonjs(), // convert CommonJS modules to ES6
+      typescript({ tsconfig: './tsconfig.json' }), // integration between rollup and typescript
+      terser(), // minify generated es bundle (uses terser under the hood)
+      banner2(() => myBanner), // add banner
+    ],
+    external: ['react', 'react-dom'],
+  },
+  // typings
+  {
+    input: 'dist/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts(), del({ targets: 'dist/types', hook: 'buildEnd' }), banner2(() => myBanner)], // roll-up .d.ts files and delete the types dir
+  },
+];
+
+export default config;
