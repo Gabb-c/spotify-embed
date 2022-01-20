4e7684346f12fe49cf73cdd37945f7d58add1d7c
Gabb-c <gdgabrieldacunha71@gmail.com>
2022-01-19 22:21:44 -0300

    test: add snapshot tests to spotify-embed component

--- src/__tests__/spotify-embed.spec.tsx
+++ src/__tests__/spotify-embed.spec.tsx
@@ -12,11 +12,27 @@ describe('Spotify Embed', () => {
     expect(embed).toBeInTheDocument();
   });
 
+  it('should match snapshot (URL)', () => {
+    render(<SpotifyEmbed src="https://open.spotify.com/track/5mslcDXYQbmOFL6s031NvN" />);
+
+    const embed = screen.queryByLabelText('spotify-embed');
+
+    expect(embed).toMatchSnapshot();
+  });
+
   it('should render and be in the document (URI)', () => {
     render(<SpotifyEmbed src="spotify:track:5mslcDXYQbmOFL6s031NvN" />);
 
     const embed = screen.queryByLabelText('spotify-embed');
 
     expect(embed).toBeInTheDocument();
+  });
+
+  it('should match snapshot (URI)', () => {
+    render(<SpotifyEmbed src="spotify:track:5mslcDXYQbmOFL6s031NvN" />);
+
+    const embed = screen.queryByLabelText('spotify-embed');
+
+    expect(embed).toMatchSnapshot();
   });
 });
