f5eaf913b3099d71676c7468fc366743b220a46d
Gabb-c <gdgabrieldacunha71@gmail.com>
2022-02-17 12:14:46 -0300

    chore: update semantic-release github config

--- src/__tests__/enums.spec.ts
+++ src/__tests__/enums.spec.ts
@@ -1,27 +1,27 @@
-import { Compact, Default, SpotifyEmbedTypes, SpotifyEmbedUrl, Themes } from '../utils/enums';
+import * as ENUMS from '../utils/enums';
 
 describe('Sizes', () => {
   it('Compact enum should match snapshot', () => {
-    expect(Compact).toMatchSnapshot();
+    expect(ENUMS.Compact).toMatchSnapshot();
   });
 
   it('Default enum should match snapshot', () => {
-    expect(Default).toMatchSnapshot();
+    expect(ENUMS.Default).toMatchSnapshot();
   });
 });
 
 describe("Embed Types and URL's", () => {
   it('SpotifyEmbedTypes enum should match snapshot', () => {
-    expect(SpotifyEmbedTypes).toMatchSnapshot();
+    expect(ENUMS.SpotifyEmbedTypes).toMatchSnapshot();
   });
 
   it('SpotifyEmbedUrl enum should match snapshot', () => {
-    expect(SpotifyEmbedUrl).toMatchSnapshot();
+    expect(ENUMS.SpotifyEmbedUrl).toMatchSnapshot();
   });
 });
 
 describe('Embed Themes', () => {
   it('SpotifyEmbedTypes enum should match snapshot', () => {
-    expect(Themes).toMatchSnapshot();
+    expect(ENUMS.Themes).toMatchSnapshot();
   });
 });
